de90d0f1ab222150c51fb7c759cc883cc9b570ee
Chris Thompson <chris_thompson@bcit.ca>
2022-10-06 12:50:59 -0700

    Hello world!

---
+++ src/ca/bcit/comp2522/lab04/HelloWorld.java
@@ -0,0 +1,17 @@
+package ca.bcit.comp2522.lab04;
+
+/**
+ * Hello world!
+ *
+ * @author BCIT
+ * @version 202230
+ */
+public class HelloWorld {
+    /**
+     * Drives the program.
+     * @param args unused
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
